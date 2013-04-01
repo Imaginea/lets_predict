364400b33f9d4f8db76ad02440313aca717f4b83
Sathishkumar Natesan <sathish.n@imaginea.com>
2013-04-01 09:17:44 +0530

    Change team order in seeds. More important

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 Tournament.create!(:name => 'IPL 6', :start_date => '03/04/2013', :end_date => '26/05/2013', :sport => 'Cricket')
-['Chennai Super Kings', 'Royal Challengers Bangalore', 'Delhi Daredevils', 'Mumbai Indians', 'Kolkata Knight Riders', 'Rajasthan Royals', 'Kings XI Punjab', 'Pune Warriors', 'Sun Risers Hyderabad'].each do |team_name|
+['Kolkata Knight Riders', 'Delhi Daredevils', 'Sun Risers Hyderabad', 'Pune Warriors', 'Rajasthan Royals', 'Chennai Super Kings', 'Kings XI Punjab', 'Royal Challengers Bangalore', 'Mumbai Indians'].each do |team_name|
   Team.create!(:name => team_name)
 end
