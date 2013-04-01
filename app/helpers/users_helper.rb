553a15fa1cabdc84b2ec2496d0e0d4515bbc560d
SuprajaSuresh <supraja.suresh.s@gmail.com>
2013-04-01 10:01:00 +0530

    fixed minor issies

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,13 @@ module UsersHelper
 
   def get_btn_class(predicted_teams, match_id, team_name)
     return "btn btn-primary btn-block" if predicted_teams[match_id].blank?
-    return "btn btn-success btn-block disabled" if predicted_teams[match_id].last == team_name
+    return "btn btn-success btn-block disabled" if predicted_teams[match_id].second == team_name
     "btn btn-block"
   end
 
   def get_icon_class(predicted_teams, match_id, team_name)
     return "icon-ok pull-right hide" if predicted_teams[match_id].blank?
-    return "icon-ok pull-right" if predicted_teams[match_id].last == team_name
+    return "icon-ok pull-right" if predicted_teams[match_id].second == team_name
     "icon-ok pull-right hide"
   end
 end
