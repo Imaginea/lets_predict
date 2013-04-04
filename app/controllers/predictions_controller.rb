cda2572a57712353a70a31170b8512e5d62c83bd
SuprajaSuresh <supraja.suresh.s@gmail.com>
2013-04-04 15:15:38 +0530

    Fixed security issue for prediction

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -4,9 +4,12 @@ class PredictionsController < ApplicationController
   def create
     predictions = params[:predictions].values + params[:non_league_predictions].values
     t_id = predictions.first[:tournament_id].to_i
+    matches = Match.where(:tournament_id => t_id).to_a.group_by(&:id)
     user_predictions = current_user.predictions.where(:tournament_id => t_id).group_by(&:match_id)
       predictions.each do |prediction|
         m_id = prediction[:match_id].to_i
+        next unless matches[m_id].try(:first).try(:can_predict?)
+        
         pred = user_predictions[m_id] || [current_user.predictions.build(
           :match_id => m_id, :tournament_id => t_id )]
         pred = pred.first
