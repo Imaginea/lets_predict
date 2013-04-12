a3065f9760f5eebd775b61f5344783631855282c
SuprajaSuresh <supraja.suresh.s@gmail.com>
2013-04-12 17:42:14 +0530

    Added URL (Still have to  optimize it)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default from: "lets-predict-noreply@imaginea.com"
+  default :from => "lets-predict-noreply@imaginea.com" 
   USER_GROUP = "all@pramati.com"
-  URL = "http://predict.imaginea.com"
   def new_tournament_email(newtournaments)
     @tournaments = newtournaments
+    @url= "http://predict.imaginea.com"
     #mail(:to => USER_GROUP, :subject => "New Tournament available for prediction")
     mail(:to => "supraja.s@imaginea.com", :subject => "Lets-Predict - New Tournament available for prediction")
   end
 
   def tournament_update_email(currenttournaments)
     @tournaments = currenttournaments
+    @url= "http://predict.imaginea.com"
     #mail(:to => USER_GROUP, :subject => "New Tournament available for prediction")
     mail(:to => "supraja.s@imaginea.com", :subject => "Lets-Predict - Tournament Update")
   end
--- config/application.rb
+++ config/application.rb
@@ -74,7 +74,7 @@ module LetsPredict
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
 
-    config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+    config.action_mailer.default_url_options = { :host => 'predict.imaginea.com' }
     
   end
 end
