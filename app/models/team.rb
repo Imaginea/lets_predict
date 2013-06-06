8dc47506b7eef144dc08b1e53d9ced7b6f6c42cf
Sathishkumar Natesan <sathish.n@imaginea.com>
2013-06-06 19:20:00 +0530

    handle team abbrevations for one word-ed teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,7 @@ class Team < ActiveRecord::Base
   has_many :matches
 
   def abbrev
-    self.name.split(' ').collect { |word| word.first }.join
+    words = self.name.split(' ')
+    words.length == 1 ? self.name.first(3) : words.collect { |w| w.first }.join
   end
 end
