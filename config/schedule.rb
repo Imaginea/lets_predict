cacac5c7f377f532e4ea14e5be2f12c54733f7b7
SuprajaSuresh <supraja.suresh.s@gmail.com>
2014-04-09 10:04:33 +0530

    Modified cron symtax

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 set :output, "log/schedular.log"
 
-every '00 14 * * *' do
-  runner "User.send_prediction_reminder"
-end 
-
-every '00 18 * * *' do
+every '00 14,18 * * *' do
   runner "User.send_prediction_reminder"
 end
